5743ac3b8cafb72d5ab92ddba044a58016d12c06
Naci Akce <n.akce@limesoda.com>
2019-03-24 20:32:44 +0100

    add rollup

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+
+const dist = 'dist';
+const bundle = 'bundle';
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+    input: 'src/index.js',
+    external: ['react'],
+    output: [
+        {
+            file: `${dist}/${bundle}.cjs.js`,
+            format: 'cjs'
+        },
+        {
+            name: 'toggleAll',
+            file: `${dist}/${bundle}.umd.js`,
+            globals: {
+                react: 'React'
+            },
+            format: 'umd'
+        },
+        {
+            file: `${dist}/${bundle}.esm.js`,
+            format: 'esm'
+        }
+    ],
+    plugins: [
+        resolve(),
+        babel({
+            exclude: 'node_modules/**'
+        }),
+        production && terser()
+    ]
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
-console.log("naci" + "oder");
+const $ = element => document.querySelector(element);
+const $$ = elements => document.querySelectorAll(elements);
+export default class Toggle {
+    constructor(config = {}) {
+        this.selector = config.selector || '[toggle]';
+        this.activeClass = config.activeClass || 'is--active';
+        this.toggleGroup = config.toggleGroup || 'toggle-group';
+        this.toggles = [].slice.call($$(this.selector));
+        if (!toggles) return;
+
+        this.body = $('body');
+        this.init();
+    }
+
+    init() {
+        this.events();
+    }
+
+    events() {
+        this.toggles.map(item => item.addEventListener('click', this.toggleItem));
+    }
+
+    toggleItem(event) {
+        event.preventDefault();
+        if (event.currentTarget.classList.contains(this.activeClass)) {
+            this.close(event.currentTarget);
+            return;
+        }
+
+        event.currentTarget.hasAttribute(this.toggleGroup) &&
+            [...$$([this.toggleGroup])]
+                .filter(e => e !== event.currentTarget && e.classList.contains(this.activeClass))
+                .forEach(e => this.close(e));
+
+        open(event.currentTarget);
+    }
+
+    open(item) {
+        item.classList.add(this.activeClass);
+        $(item.getAttribute('toggle')).classList.add(this.activeClass);
+    }
+
+    close(item) {
+        item.classList.remove(this.activeClass);
+        $(item.getAttribute('toggle')).classList.remove(this.activeClass);
+    }
+}
